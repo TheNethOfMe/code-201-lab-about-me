159e1f4ed57500783ae475dfd169643eaf17561c
neth <nethwebdev@gmail.com>
2018-08-02 10:24:25 -0700

    simplified some of the js, much cleaner

--- js/app.js
+++ js/app.js
@@ -13,7 +13,9 @@ const questionList =
     'Do you think Neth likes kids?',
     'Is Neth too good to eat at McDonalds?',
     'Does Neth have a favorite sport?',
-    'Is Neth\'s opinion of billionairs a positive one?'
+    'Is Neth\'s opinion of billionairs a positive one?',
+    'How many addresses do you think Neth has had in his life?',
+    'Name a state, besides Washington, you think Neth has lived in. There are two possible answers.'
   ];
 
 // list of responses to return to user
@@ -30,84 +32,79 @@ const responseList =
 let score = 0;
 const userName = prompt('Hey, can I get your name?');
 alert(`Nice to meet you ${userName}. Let's see how much you know about Neth.`);
+
 // where all the magic happens
 for (let i = 0; i < 7; i++) {
+  console.log(`QUESTION ${i + 1}`);
+  console.log(questionList[i]);
   if (i === 5) {
-    console.log('QUESTION 6');
     // if i = 5, start the sixth quesiton
     let chances = 0;
     let isCorrect = false;
-    while (chances < 4) {
+    while (chances < 4 && !isCorrect) {
       // send user the question and let them know how many guesses they have left
-      const questionSix = prompt(
-        `How many addresses do you think Neth has had in his life? You have ${4 - chances} ${chances === 3 ? 'guess' : 'guesses'}.`
-      );
+      const questionSix = parseInt(prompt(questionList[i]));
       // check if user's guess was correct, too high, or too low
-      console.log(`The user has guessed ${questionSix}`);
-      if (questionSix === '8') {
-        // on correct guess, chances is set to 4 to escape while loop
+      if (questionSix === 8) {
+        // on correct guess, we escape the loop
         alert('Wow, you got it');
         console.log(`The user guessed correctly after guessing ${chances + 1} ${chances === 0 ? 'time' : 'times'}.`);
-        chances = 4;
         isCorrect = true;
         score++;
+        // break;
         // incorrect guesses increment the chances by one and alert user with feedback
       } else if (questionSix < 8) {
-        chances < 3 && alert('Nope. It\'s more than that.');
         chances++;
+        chances < 4 && alert(`Nope. It's more than that. You have ${4 - chances} ${chances === 3 ? 'guess' : 'guesses'} left.`);
       } else if (questionSix > 8) {
-        chances < 3 && alert('No, not that many.');
         chances++;
+        chances < 4 && alert(`No, not that many. You have ${4 - chances} ${chances === 3 ? 'guess' : 'guesses'} left.`);
       } else {
-        chances < 3 && alert('My dude, you have to enter a number.');
         chances++;
+        chances < 4 && alert(`My dude, you have to enter a number. You have ${4 - chances} ${chances === 3 ? 'guess' : 'guesses'} left.`);
       }
     }
     // this alert displays if user failed to guess correctly
     !isCorrect && alert('Sorry. The correct answer was 8.');
     !isCorrect && console.log('The user failed to guess correctly.');
   } else if (i === 6) {
-    console.log('FINAL QUESTION');
     // if i === 6, start the final question
     let chances = 0;
     let isCorrect = false;
-    while (chances < 4) {
+    while (chances < 4 && !isCorrect) {
       // send user the question and let them know how many guesses they have left
-      const questionSeven = prompt(
-        `What other state do you think Neth has lived in? You have ${4 - chances} ${chances === 3 ? 'guess' : 'guesses'}.`
-      );
+      const questionSeven = prompt(questionList[i]);
       // check if users's guess was correct
-      if (finalQuestionResponse.indexOf(questionSeven.toLowerCase()) >= 0) {
+      if (finalQuestionResponse.includes(questionSeven.toLowerCase())) {
         // correct response escapes while loop and alerts user that they are correct
         alert('You got one! Way to go. The answers were Idaho and Missouri');
         console.log(`The user guessed correctly after guessing ${chances + 1} ${chances === 0 ? 'time' : 'times'}.`);
-        chances = 4;
         isCorrect = true;
         score++;
+        // break;
       } else {
         // incorrecct response alerts the user and increments chances by one
-        chances < 3 && alert('Nope. Not one of them.');
         chances++;
+        chances < 4 && alert(`Nope. Not one of them. You have ${4 - chances} ${chances === 3 ? 'guess' : 'guesses'} left.`);
       }
     }
     // this alert displays if user failed to guess correctly
     !isCorrect && alert('Sorry. The correct answers were Idaho and Missouri.');
     !isCorrect && console.log('The user failed to guess correctly.');
   } else {
+    // for questions one through five
     // gets a question from the list and saves the user's response
     const userResponse = prompt(questionList[i]);
-    console.log(`QUESTION ${i + 1}`);
-    console.log(`Question: ${questionList[i]}`);
     console.log(`Response: ${userResponse}`);
     let result;
     let response;
     // find out if the user gave the correct answer
     if (answerKey[i]) {
       // the correct answer was yes
-      result = posResponse.indexOf(userResponse.toLowerCase()) >= 0 ? true : false;
+      result = posResponse.includes(userResponse.toLowerCase());
     } else {
       // the correct answer was no
-      result = negResponse.indexOf(userResponse.toLowerCase()) >= 0 ? true : false;
+      result = negResponse.includes(userResponse.toLowerCase());
     }
     // create the response to the user based on their answer
     if (result) {
